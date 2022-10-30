30d558b5c0c84c4c0e8d30f43741e681cf1e4e87
Marko Projovic <marko.projovic@crayon.com>
2022-10-30 16:52:14 +0100

    adding domain entities

---
+++ src/API/Todo.Api/Todo.Api.Domain/Entities/Task.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Todo.Api.Domain.Entities
+{
+    public class Task
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The max length of Title field is 50 characters!")]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The max length of Description field is 100 characters!")]
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        public DateTimeOffset Deadline { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public int TaskListId { get; set; }
+
+        [ForeignKey("TaskListId")]
+
+        public virtual TaskList TaskList { get; set; }
+    }
+}
---
+++ src/API/Todo.Api/Todo.Api.Domain/Entities/TaskList.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace Todo.Api.Domain.Entities
+{
+    public class TaskList
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The max length of Title field is 50 characters!")]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The max length of Description field is 100 characters!")]
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "DailyList is required field!")]
+        public DateTimeOffset DailyList { get; set; }
+
+        [Required(ErrorMessage = "CreatedByUserId is required field")]
+        [MaxLength(450)]
+        public string CreatedByUserId { get; set; }
+
+        [Required(ErrorMessage = "TimeZone is required field")]
+        [MaxLength(50)]
+        public string TimeZone { get; set; }
+    }
+}
