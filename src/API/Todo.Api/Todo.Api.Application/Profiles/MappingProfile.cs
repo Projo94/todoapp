6b82fedbe74dc8e52a08f5fc1dcf722e1b9b6376
Marko Projovic <marko.projovic@crayon.com>
2022-10-31 00:00:13 +0100

    automapper profile defined

---
+++ src/API/Todo.Api/Todo.Api.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Todo.Api.Application.Features.Queries.GetTaskList;
+using Todo.Api.Application.Features.TaskList.Queries.GetTaskList;
+using Todo.Api.Application.Filtering;
+using Entities = Todo.Api.Domain.Entities;
+
+namespace Todo.Api.Application.Profiles
+{
+    class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Entities.TaskList, TaskListVm>()
+                .ForMember(dest => dest.DailyList, opt => opt.MapFrom(src => TimeZoneInfo.ConvertTimeBySystemTimeZoneId(src.DailyList, src.TimeZone).DateTime));
+
+            CreateMap<ListFilter, Filtering.ListFilterDto>().ReverseMap();
+            CreateMap<ListFilter, Models.Filters.ListFilterDto>().ReverseMap();
+
+            CreateMap<TaskListDto, Entities.TaskList>().ReverseMap();
+        }
+    }
+}
