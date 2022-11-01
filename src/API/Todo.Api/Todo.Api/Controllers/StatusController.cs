b75bb8cffe5b745afdaff74c65ccbd7c5d582686
Marko Projovic <marko.projovic@crayon.com>
2022-11-02 00:00:16 +0100

    ping server endpoints added

---
+++ src/API/Todo.Api/Todo.Api/Controllers/StatusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        [HttpPost]
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
