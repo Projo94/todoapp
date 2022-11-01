e091e6e76dcc29b9a717aa9e3ded17d7b3ea44c4
Marko Projovic <marko.projovic@crayon.com>
2022-11-01 21:41:58 +0100

    added TimezoneController

---
+++ src/API/Todo.Api/Todo.Api/Controllers/TimezoneController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Api.Controllers
+{
+    [EnableCors("Open")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimezoneController : BaseApiController
+    {
+        [HttpGet("all", Name = "GetTimezones")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public ActionResult<List<string>> GetTimezones()
+        {
+            var timezones = new List<string>();
+
+            foreach (TimeZoneInfo z in TimeZoneInfo.GetSystemTimeZones())
+            {
+                timezones.Add($"{z.Id}|||||{z.BaseUtcOffset}");
+            }
+
+            return Ok(timezones);
+        }
+    }
+}
+
